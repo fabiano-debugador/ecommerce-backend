ada672d768dacb8bcf72e2ad1db2492e8e17cf8c
fabiano-debugador <fabiano_the_beast@hotmail.com>
2023-05-28 12:57:29 -0300

    first commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUsers()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
+  }
+}
